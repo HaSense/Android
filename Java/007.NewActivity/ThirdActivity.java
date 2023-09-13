28e3bac1189d231dcb4603073acb2e47f09580ff
Virtual Sense <virtual_sense@naver.com>
2023-09-14 06:22:31 +0900

    Create ThirdActivity.java

---
+++ Java/007.NewActivity/ThirdActivity.java
@@ -0,0 +1,20 @@
+package com.example.newactivitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        Button btnFinish = findViewById(R.id.btnFinish);
+        btnFinish.setOnClickListener(view -> {
+            finish();
+        });
+    }
+}
