bf5406453a93a2744122ff9787f514533d05a3f2
Virtual Sense <virtual_sense@naver.com>
2023-09-14 06:22:06 +0900

    Create SecondActivity.java

---
+++ Java/007.NewActivity/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.newactivitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btnReturn = findViewById(R.id.btnResutn);
+        btnReturn.setOnClickListener(view -> {
+            finish();
+        });
+    }
+}
