7a19929b388a2f5509f91d88e6f1abd5bc46e24d
Virtual Sense <virtual_sense@naver.com>
2023-09-15 15:54:29 +0900

    Create Person.java

---
+++ Java/015.CustomAdapter/Person.java
@@ -0,0 +1,13 @@
+package com.example.customadapter;
+
+public class Person
+{
+    public int icon;
+    public String name;
+
+    public Person(int icon, String name)
+    {
+        this.icon = icon;
+        this.name = name;
+    }
+}
