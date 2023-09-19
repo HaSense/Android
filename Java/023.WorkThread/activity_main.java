9db7a6f80b855cef3f533e4c95838b9c19a4acde
Virtual Sense <virtual_sense@naver.com>
2023-09-19 15:59:12 +0900

    Create activity_main.java

---
+++ Java/023.WorkThread/activity_main.java
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.appcompat.widget.LinearLayoutCompat
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+
+
+    android:orientation="vertical"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
+
+    <TextView
+        android:id="@+id/textView1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="업로드를 시작하려면 다음 버튼을 누르세요" />
+
+    <Button
+        android:id="@+id/btnUpload"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="업로드" />
+</androidx.appcompat.widget.LinearLayoutCompat>
